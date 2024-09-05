aa65139f63af27512073ac313cef58aaa544adbe
Onelevenvy <qingxin1114@163.com>
2024-09-05 13:37:42 +0800

    enhance: optimize the layout of team settings page

--- web/src/components/Teams/TeamSettings.tsx
+++ web/src/components/Teams/TeamSettings.tsx
@@ -2,10 +2,11 @@ import { useParams } from "next/navigation";
 import EditAgent from "../Members/EditAgent";
 import { useQuery } from "react-query";
 import { ApiError, MembersService } from "@/client";
-import { Box, Button, Flex, Spinner } from "@chakra-ui/react";
+import { Box, Button, Flex, Spinner, Text } from "@chakra-ui/react";
 import { useRef } from "react";
 import useCustomToast from "@/hooks/useCustomToast";
 import ChatMain from "../Playground/ChatMain";
+import { MdBuild } from "react-icons/md";
 
 export default function TeamSettings() {
   const { teamId } = useParams() as { teamId: string };
@@ -64,11 +65,45 @@ export default function TeamSettings() {
               ))}
             </Box>
             <Box w="full" display={"flex"} flexDirection={"column"}>
-              <Box ml={"auto"} mr="2">
-                <Button onClick={triggerSubmit}>Save</Button>
-              </Box>
-              <Box w="full" h="full" bg={"white"} borderRadius={"lg"} pt="8">
-                <ChatMain isPlayground={false} />
+              <Box
+                w="full"
+                h="full"
+                bg="white"
+                borderRadius={"lg"}
+                display={"flex"}
+                flexDirection={"column"}
+              >
+                <Box
+                  display={"flex"}
+                  flexDirection={"row"}
+                  justifyContent={"space-between"}
+                  alignItems={"center"}
+                >
+                  <Text mt="5" ml="5" fontSize={"xl"} fontWeight={"bold"}>
+                    调试预览
+                  </Text>
+                  <Button
+                    mt="5"
+                    mr="5"
+                    bg={"#2762e7"}
+                    borderRadius={"md"}
+                    onClick={triggerSubmit}
+                    _hover={{ backgroundColor: "#1c86ee" }}
+                    rightIcon={<MdBuild color={"white"} />}
+                  >
+                    <Text color={"white"}>发布</Text>
+                  </Button>
+                </Box>
+                <Box
+                  display={"flex"}
+                  w="full"
+                  h="full"
+                  maxH={"full"}
+                  bg={"white"}
+                  // mt="10"
+                >
+                  <ChatMain isPlayground={false} />
+                </Box>
               </Box>
             </Box>
           </Box>
