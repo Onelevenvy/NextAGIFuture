fed409b487b1d938feab07b1caa5d1fc4b19a063
Onelevenvy <49232224+Onelevenvy@users.noreply.github.com>
2024-09-07 10:45:15 +0800

    chore: update i18n file (#16)

--- web/src/utils/classnames.ts
+++ web/src/utils/classnames.ts
@@ -1,8 +1,8 @@
-import { twMerge } from 'tailwind-merge'
-import cn from 'classnames'
+import { twMerge } from "tailwind-merge";
+import cn from "classnames";
 
 const classNames = (...cls: cn.ArgumentArray) => {
-  return twMerge(cn(cls))
-}
+  return twMerge(cn(cls));
+};
 
-export default classNames
+export default classNames;
--- web/src/utils/timezone.ts
+++
@@ -1,7 +0,0 @@
-import tz from './timezone.json'
-
-type Item = {
-  value: number | string
-  name: string
-}
-export const timezones: Item[] = tz
