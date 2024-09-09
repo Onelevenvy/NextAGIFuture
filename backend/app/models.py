127eb71ac8ce0736d89c6f9f5c7a81a94bed8bf2
Onelevenvy <qingxin1114@163.com>
2024-09-09 09:42:17 +0800

    feat：get usr language form usr table

--- backend/app/models.py
+++ backend/app/models.py
@@ -83,8 +83,10 @@ class UpdatePassword(SQLModel):
     current_password: str
     new_password: str
 
+
 class UpdateLanguageMe(SQLModel):
     language: str = Field(default="en-US")
+
 
 # Database model, database table inferred from class name
 class User(UserBase, table=True):
@@ -94,6 +96,7 @@ class User(UserBase, table=True):
     skills: list["Skill"] = Relationship(back_populates="owner")
     uploads: list["Upload"] = Relationship(back_populates="owner")
     language: str = Field(default="en-US")
+
 
 # Properties to return via API, id is always required
 class UserOut(UserBase):
--- web/src/components/Settings/LanguagePage/index.tsx
+++ web/src/components/Settings/LanguagePage/index.tsx
@@ -2,88 +2,59 @@
 
 import { ApiError } from "@/client";
 import { UpdateLanguageMe } from "@/client/models/UpdateLanguageMe";
-import { UserOut } from "@/client/models/UserOut";
 import { UsersService } from "@/client/services/UsersService";
 import useAuth from "@/hooks/useAuth";
 import useCustomToast from "@/hooks/useCustomToast";
-import useLocaleStore from "@/store/localeStore";
-import { Select, useColorModeValue } from "@chakra-ui/react";
-import React, { ChangeEvent, useState } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
+import {
+  Container,
+  FormControl,
+  FormErrorMessage,
+  Select,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import React, { ChangeEvent } from "react";
 import { useMutation, useQueryClient } from "react-query";
+
 export default function LanguagePage() {
-  const { setLocaleValue } = useLocaleStore();
-  const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
-    setLocaleValue(event.target.value);
-  };
-
-
   const queryClient = useQueryClient();
-  const color = useColorModeValue("inherit", "ui.white");
   const showToast = useCustomToast();
-  const [editMode, setEditMode] = useState(false);
-  const { user: currentUser } = useAuth();
-  const {
-    register,
-    handleSubmit,
-    reset,
-    getValues,
-    formState: { isSubmitting, errors, isDirty },
-  } = useForm<UserOut>({
-    mode: "onBlur",
-    criteriaMode: "all",
-    defaultValues: {
-      full_name: currentUser?.full_name,
-      email: currentUser?.email,
-    },
-  });
 
-  const toggleEditMode = () => {
-    setEditMode(!editMode);
+  const { currentUser } = useAuth();
+
+  const mutation = useMutation(
+    (language: string) =>
+      UsersService.updateUserLanguage({ requestBody: { language } }),
+    {
+      onSuccess: () => {
+        showToast("Success!", "User updated successfully.", "success");
+      },
+      onError: (err: ApiError) => {
+        const errDetail = err.body?.detail;
+        showToast("Something went wrong.", `${errDetail}`, "error");
+      },
+      onSettled: () => {
+        queryClient.invalidateQueries("users");
+        queryClient.invalidateQueries("currentUser");
+      },
+    }
+  );
+
+  const onChangeLanguage = (event: ChangeEvent<HTMLSelectElement>) => {
+    const selectedLanguage = event.target.value;
+    mutation.mutate(selectedLanguage);
   };
-
-  const updateInfo = async (data: UpdateLanguageMe) => {
-    await UsersService.updateUserLanguage({ requestBody: data });
-  };
-
-  const mutation = useMutation(updateInfo, {
-    onSuccess: () => {
-      showToast("Success!", "User updated successfully.", "success");
-    },
-    onError: (err: ApiError) => {
-      const errDetail = err.body?.detail;
-      showToast("Something went wrong.", `${errDetail}`, "error");
-    },
-    onSettled: () => {
-      queryClient.invalidateQueries("users");
-      queryClient.invalidateQueries("currentUser");
-    },
-  });
-
-  const onSubmit: SubmitHandler<UpdateLanguageMe> = async (data) => {
-    mutation.mutate(data);
-  };
-
-  const onCancel = () => {
-    reset();
-    toggleEditMode();
-  };
-
-
-
-
-
 
   return (
-    <>
-      <Select defaultValue={"zh-Hans"} onChange={handleChange}>
-
-        <option value="zh-Hans">中文-简体</option>
-        <option value="en-US">English-US</option>
-
-     
-
-      </Select>
-    </>
+    <Container maxW="full">
+      <FormControl mt={8}>
+        <Select
+          defaultValue={currentUser?.language}
+          onChange={onChangeLanguage}
+        >
+          <option value="zh-Hans">中文-简体</option>
+          <option value="en-US">English-US</option>
+        </Select>
+      </FormControl>
+    </Container>
   );
 }
